5f61c0a15d68145052702f247843998bc7f56a4f
vaibhav bhalla <vaibhav.bhalla@sourcefuse.com>
2025-06-05 17:45:54 +0530

    fix(subscription-service): missed Export of Sequelize Component in Index file
    GH-82

--- services/tenant-management-service/src/index.ts
+++ services/tenant-management-service/src/index.ts
@@ -3,6 +3,7 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 export * from './component';
+export * from './sequelize-component';
 export * from './types';
 export * from './keys';
 export * from './repositories';
